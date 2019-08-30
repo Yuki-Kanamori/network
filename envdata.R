8f04b4210fb5e7c3e3f4b78575b7436ab4597cb4
Yuki-Kanamori <y.kanamory@gmail.com>
2019-08-30 10:45:57 +0900

    depth map

--- envdata.R
+++ envdata.R
@@ -1,4 +1,4 @@
-install.packages("xlsx", dep=T)
+#install.packages("xlsx", dep=T)
 require(xlsx)
 require(openxlsx)
 require(tidyr)
@@ -46,6 +46,7 @@ col = mutate(col, layer = NA, env = "colour")
 # colnames(wt)
 # colnames(col)
 
+summary(wt)
 env = rbind(do, sal, dep, wt, col)
 #env = env[-126004, ]
 
@@ -61,14 +62,19 @@ env = gather(env, key = st, value = value, 2:21)
 summary(env)
 write.csv(env, "env_data.csv", fileEncoding = "CP932")
 
+setwd("/Users/Yuki/Dropbox/Network/revised_data")
+env = read.csv("env_data.csv", fileEncoding = "CP932")
 require(plyr)
 env2 = env
 env2 = na.omit(env2)
+summary(env2)
 #env2 = ddply(env2, .(year, month, env, layer), summarize, mean = mean(value))
 env3 = ddply(env2, .(year, env, layer), summarize, mean = mean(value))
 n_sal = filter(env2, env == "salinity" & mean == max(mean))
 env4 = ddply(env2, .(time, env, layer), summarize, mean = mean(value))
 env5 = ddply(env2, .(year, month, env, layer), summarize, mean = mean(value))
+
+
 
 require(ggplot2)
 g = ggplot(env3, aes(x = year, y = mean, colour = factor(layer), group = factor(layer)))
@@ -92,4 +98,65 @@ th = theme(panel.grid.major = element_blank(),
            axis.text.y = element_text(size = rel(1.5)),
            legend.title = element_text(size = 13))
 g+p+l+f+th+theme_bw()
+
+
+
+# depth ---------------------------------------------------------
+require(xlsx)
+require(openxlsx)
+require(tidyr)
+require(dplyr)
+require(plyr)
+
+setwd("/Users/Yuki/Dropbox/Network/envdata")
+dep = read.xlsx("海深2_1987-2018.xlsx", 1)
+yr = rep(1987:2018, each = 12)
+mo = rep(seq(1,12,1), 32)
+time = data_frame(year = yr, month = formatC(mo, width=2, flag="0"))
+time = time[-c(383,384), ] %>% mutate(time = paste(year, month, sep = "_"))
+dep = cbind(dep, time)
+dep = dep %>% gather(key = st, value = value, 2:21) %>% filter(!is.na(value)) 
+y_dep = ddply(dep, .(st, year), summarize, mean = mean(value))
+
+g = ggplot(y_dep, aes(x = year, y = mean, group = st))
+p = geom_point()
+l = geom_line()
+f = facet_wrap( ~ st, ncol = 2, scales = "free")
+g+p+l+f
+
+m_dep = ddply(y_dep, .(st), summarize, mean = mean(mean))
+setwd("/Users/Yuki/Dropbox/Network/revised_data")
+site = read.csv("env_site.csv", fileEncoding = "CP932")
+require(dplyr)
+head(site)
+site = mutate(site, lat = ifelse(site$N > 3500, 35+(site$N-3500)/60, 34+(site$N-3400)/60), 
+              lon = ifelse(site$E > 14000, 140+(site$E-14000)/60, 139+(site$E-13900)/60)) %>% select(st, r_st, lon, lat)
+head(m_dep,3)
+head(site,3)
+m_dep = merge(m_dep, site, by = "st")
+#m_dep = m_dep[, -2]
+
+
+require(maps)
+require(mapdata)
+require(ggplot2)
+p <- ggplot() + coord_fixed() +
+  xlab("Longitude") + ylab("Latitude")
+world_map <- map_data("world")
+jap <- subset(world_map, world_map$region == "Japan")
+jap_cog <- jap[jap$lat > 35 & jap$lat < 38 & jap$long > 139 & jap$long < 141, ]
+t2 <- p + geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")+ coord_map(xlim = c(139.5, 140.3), ylim = c(34.9, 35.75))
+
+p <- geom_point(data = m_dep, aes(x = lon, y = lat, colour = mean), shape = 16, size = 5)
+s <- scale_colour_gradientn(colours = c("blue", "cyan", "green", "yellow", "orange", "red"))
+lb <- labs(color = "Mean depth", title = "")
+th <- theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.text.x = element_text(size = rel(1), angle = 90, hjust = 1), #x軸メモリ
+            axis.text.y = element_text(size = rel(1)), #y軸メモリ
+            axis.title.x = element_text(size = rel(1)), #x軸タイトル
+            axis.title.y = element_text(size = rel(1)),
+            #strip.text.y = element_text(size = 100),
+            legend.title = element_text(size = 10))
+t2+p+s+lb+th+theme_bw()
 
